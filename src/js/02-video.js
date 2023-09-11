928fae48f15f773b0ef5840413cb2efa05b03657
Yurii <gapirov21@gmail.com>
2023-09-12 00:08:11 +0300

    hw8-task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
+const VPCT_KEY = "videoplayer-current-time"; //робимо константу для розміщення в коді
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe); // створюємо новий плеєр і взаємодіємо з ним
 
 const onPlay = function (event) {
-    localStorage.setItem("videoplayer-current-time", event.seconds);  //зберігаємо час(місце) відтворення відео
+    localStorage.setItem(VPCT_KEY, event.seconds);  //зберігаємо час(місце) відтворення відео
 };
 
 player.on('timeupdate', throttle(onPlay, 1000)); //відстежуємо подію оновлення часу, викликаємо функцію onplay і ставимо інтервал 1сек
 
-const setTime = localStorage.getItem("videoplayer-current-time");  //отримали час(місце) відтворення
+const setTime = localStorage.getItem(VPCT_KEY);  //отримали час(місце) відтворення
 
 player.setCurrentTime(setTime);  //передали для відтворення
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+
+const STORAGE_KEY = 'feedback-msg'; //робимо константу для розміщення в коді при повторенні ключа
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    emailInput: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+populateTextarea();
+
+refs.form.addEventListener('submit', onFormSubmit);
+// refs.emailInput.addEventListener('input', throttle(onTextareaInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+let formdata = {
+    email: '',
+    message: '',
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault(); //блокуєм перезавантаження сторінки після сабміту
+    formData.email = refs.email.value;
+    formData.message = refs.message.value;
+    evt.currentTarget.reset(); //скидуєм значення інпут
+    localStorage.removeItem(STORAGE_KEY) //скидуємо значення локала після сабміта
+
+}
+
+
+function onTextareaInput(evt) {
+    const message = evt.target.value; //ставимо саме таргет, бо бібліотека свариться на CurrentTarget через застарілу інфо
+    const email = refs.emailInput.value;
+
+    const data = {message, email}; //створюємо обєкт для передачі меседжа і імейла
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+//отримуємо значення зі сховища при оновленні сторінки якщо там була дані
+function populateTextarea() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const {message, email} = JSON.parse(savedData); //розпарсили дані об*єкта
+    if (savedData) {
+        // console.log(savedMessage);
+        
+        refs.textarea.value = message; 
+        refs.emailInput.value = email;
+    }
+}
