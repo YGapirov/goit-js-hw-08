f0de75b35fdf77a93ccd43a81e10f8105ce3c745
Yurii <gapirov21@gmail.com>
2023-09-12 10:17:43 +0300

    hw8-taks3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -17,19 +17,22 @@ refs.form.addEventListener('submit', onFormSubmit);
 // refs.emailInput.addEventListener('input', throttle(onTextareaInput, 500));
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
-let formdata = {
+let formData = {
     email: '',
-    message: '',
+    message: '',   //
 };
 
 function onFormSubmit(evt) {
     evt.preventDefault(); //блокуєм перезавантаження сторінки після сабміту
-    formData.email = refs.email.value;
-    formData.message = refs.message.value;
+    formData.email = refs.emailInput.value;
+    formData.message = refs.textarea.value;
     evt.currentTarget.reset(); //скидуєм значення інпут
     localStorage.removeItem(STORAGE_KEY) //скидуємо значення локала після сабміта
 
-}
+    console.log('Email:', formData.email);
+    console.log('Message:', formData.message);
+
+};
 
 
 function onTextareaInput(evt) {
@@ -39,16 +42,17 @@ function onTextareaInput(evt) {
     const data = {message, email}; //створюємо обєкт для передачі меседжа і імейла
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-}
+};
 
 //отримуємо значення зі сховища при оновленні сторінки якщо там була дані
 function populateTextarea() {
     const savedData = localStorage.getItem(STORAGE_KEY);
-    const {message, email} = JSON.parse(savedData); //розпарсили дані об*єкта
+     //розпарсили дані об*єкта
     if (savedData) {
         // console.log(savedMessage);
-        
+        const {message, email} = JSON.parse(savedData);
         refs.textarea.value = message; 
         refs.emailInput.value = email;
     }
-}
+};
+
