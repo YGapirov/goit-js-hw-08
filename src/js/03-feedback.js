28059553107868801e37ebabd5aab75e40f7cd5f
Yurii <gapirov21@gmail.com>
2023-09-13 22:39:28 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,64 +1,4 @@
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state'; //робимо константу для розміщення в коді при повторенні ключа
-
-// const refs = {
-//     form: document.querySelector('.feedback-form'),
-//     emailInput: document.querySelector('.feedback-form input[name="email"]'),
-//     messageInput: document.querySelector('.feedback-form textarea'),
-// };
-
-// refs.messageInput.addEventListener('input', throttle(onFormInput, 500));
-
-// populateTextarea();
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onFormInput() {
-//     const formData = {
-//         email: refs.emailInput.value,
-//         message: refs.messageInput.value,
-// }
-// localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// };
-
-// //отримуємо значення зі сховища при оновленні сторінки якщо там була дані
-// function populateTextarea() {
-//     const savedData = localStorage.getItem(STORAGE_KEY);
-//      //розпарсили дані об*єкта
-//     if (savedData) {
-//         // console.log(savedMessage);
-//         const {email, message} = JSON.parse(savedData);
-//         refs.emailInput.value = email; 
-//         refs.messageInput.value = message;
-//     }
-//    };
-
-// function onFormSubmit(evt) {
-//     evt.preventDefault(); //блокуєм перезавантаження сторінки після сабміту
-     
-//     const formData = {
-//         email: refs.emailInput.value,
-//         message: refs.messageInput.value,
-//     };
-
-//     if (formData.email === '' || formData.message === '') {
-//         alert('Please fill in all the fields!');
-//         return; 
-//     }
-//      localStorage.removeItem(STORAGE_KEY);  // Очищаємо сховище після сабміта
-
-//    refs.emailInput.value = '';    // Очищаємо поля форми
-//    refs.messageInput.value = '';
-
-//     console.log('Submitted:', formData);
-// }
-
-
-
 import throttle from 'lodash.throttle';
-
-
 
 const STORAGE_KEY = 'feedback-form-state'; //робимо константу для розміщення в коді при повторенні ключа
 
@@ -98,7 +38,6 @@ function onFormSubmit(evt) {
 
 }
 
-
 function onFormInput(evt) {
     const email = refs.emailInput.value;
     const message = evt.target.value; //ставимо саме таргет, бо бібліотека свариться на CurrentTarget через застарілу інфо
@@ -108,8 +47,6 @@ function onFormInput(evt) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
-
-
 //отримуємо значення зі сховища при оновленні сторінки якщо там була дані
 function populateTextarea() {
     const savedData = localStorage.getItem(STORAGE_KEY);
@@ -118,11 +55,8 @@ function populateTextarea() {
         // console.log(savedMessage);
         const {email, message} = JSON.parse(savedData);
         refs.emailInput.value = email;
-        refs.textarea.value = message;
-        
-        
-    }
-   
+        refs.textarea.value = message;     
+    } 
 };
 
    
